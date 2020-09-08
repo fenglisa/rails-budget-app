a8abea7cecb98ce4bdb7cdc5108eb3603f54b9ab
fenglisa <lisafeng10@gmail.com>
2020-09-08 17:06:12 -0400

    added benefits model

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,5 +2,6 @@ class Card < ApplicationRecord
   validates :last_digits, numericality: true, length: { is: 4 }
   belongs_to :user
   has_many :purchases
+  has_many :benefits
   has_many :budgets, through: :purchases
 end
