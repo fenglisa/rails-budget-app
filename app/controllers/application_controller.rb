1bcc17e47ec61981732b9de23929c303ea3685c6
fenglisa <lisafeng10@gmail.com>
2020-09-08 09:14:26 -0400

    helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in
+  before_action :redirect_if_logged_out
+
   def current_user
-    @user ||= User.find(session[:user_id])
+    @user ||= User.find_by_id(session[:user_id])
+  end
+
+  def logged_in
+    !!session[:user_id]
+  end
+
+  def redirect_if_logged_out
+    unless logged_in
+      redirect_to root_path
+    end
   end
 end
