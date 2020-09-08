6f4b779c06477a3e61ddeec2649bf9ad257750ac
fenglisa <lisafeng10@gmail.com>
2020-09-08 09:13:28 -0400

    added sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  skip_before_action :redirect_if_logged_out
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to home_path
+    else
+      @error = 'Incorrect username and/or password'
+      render :new
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'static#index'
+
+  get '/signin', to: 'sessions#new', as: 'signin'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new', as: 'signup'
+  get '/home', to: 'users#show'
+
+  resources :users, except: [:new, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
