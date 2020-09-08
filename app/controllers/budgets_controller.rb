429f6ddebec508322c94aa327b558dca500e10af
fenglisa <lisafeng10@gmail.com>
2020-09-08 18:32:52 -0400

    budgets CRUD

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,2 +1,42 @@
 class BudgetsController < ApplicationController
+  def new
+    @budget = Budget.new
+  end
+
+  def create
+    @budget = current_user.budgets.build(budget_params)
+    if @budget.save
+      redirect_to home_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @budget = current_user.budgets.find_by_id(params[:id])
+  end
+
+  def edit
+    @budget = current_user.budgets.find_by_id(params[:id])
+  end
+
+  def update
+    @budget = current_user.budgets.find_by_id(params[:id])
+    if @budget.update!(budget_params)
+      redirect_to home_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @budget = current_user.budgets.find_by_id(params[:id])
+    @budget.destroy
+    redirect_to home_path
+  end
+
+  private
+  def budget_params
+    params.require(:budget).permit(:name, :amount, :month, :user_id)
+  end
 end
--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,4 +1,5 @@
 class Budget < ApplicationRecord
+  validates :amount, numericality: true
   belongs_to :user
   has_many :purchases
   has_many :cards, through: :purchases
