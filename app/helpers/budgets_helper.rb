81403692de03a0822eb6ca76b4ec1ba0796129e1
fenglisa <lisafeng10@gmail.com>
2020-09-10 14:35:16 -0400

    cashback earned helper

--- app/helpers/budgets_helper.rb
+++ app/helpers/budgets_helper.rb
@@ -1,2 +1,26 @@
 module BudgetsHelper
+
+  def budget_cards(budget)
+    budget.cards.uniq
+  end
+
+  def sort_purchases_by_card(budget, card)
+    card_purchases = []
+    budget.purchases.each{|p| card_purchases << p.amount if p.card_id == card.id}
+    card_purchases
+  end
+
+  def cash_back_earned(budget)
+    cash_back_earned = []
+    budget_cards(budget).each do |c|
+      card_benefits_categories = []
+      c.benefits.each {|b| card_benefits_categories << b.category}
+      if card_benefits_categories.include?(budget.name)
+        benefit = c.benefits.find_by(category: (budget.name))
+        cash_back_earned << ((benefit.cash_back)/100)*((sort_purchases_by_card(budget, c)).sum)
+      end
+    end
+    cash_back_earned.sum
+  end
+
 end
