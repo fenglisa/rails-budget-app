8552f752f99c15fd66f1508f79f5f19c99dc049a
fenglisa <lisafeng10@gmail.com>
2020-09-10 10:29:34 -0400

    benefits CRUD

---
+++ app/controllers/benefits_controller.rb
@@ -0,0 +1,41 @@
+class BenefitsController < ApplicationController
+  def new
+    @card = Card.find_by_id(params[:card_id])
+    @benefit = Benefit.new
+  end
+
+  def create
+    @card = Card.find_by_id(params[:card_id])
+    @benefit = current_user.benefits.build(benefit_params)
+    @benefit.card_id = params[:card_id]
+    if @benefit.save
+      redirect_to @benefit.card
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @benefit = current_user.benefits.find_by_id(params[:id])
+  end
+
+  def update
+    @benefit = current_user.benefits.find_by_id(params[:id])
+    if @benefit.update(benefit_params)
+      redirect_to @benefit.card
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @benefit = current_user.benefits.find_by_id(params[:id])
+    @benefit.destroy
+    redirect_to home_path
+  end
+
+  private
+  def benefit_params
+    params.require(:benefit).permit(:cash_back, :category)
+  end
+end
--- app/models/benefit.rb
+++ app/models/benefit.rb
@@ -1,3 +1,13 @@
 class Benefit < ApplicationRecord
+  validates :cash_back, presence: true
+  validates :category, presence: true
   belongs_to :card
+
+  def categories
+    ['Gas','Groceries','Restaurants','Other']
+  end
+
+  def percentage_cash_back
+    [1,1.5,2,3,5,6]
+  end
 end
