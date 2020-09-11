bb938e759e3d547177d09b1305ec453c071f2c28
fenglisa <lisafeng10@gmail.com>
2020-09-11 15:26:39 -0400

    current month budgets scope

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,20 +1,17 @@
 class Budget < ApplicationRecord
   validates :name, presence: true
-  include ActiveModel::Validations
-  validates_with BudgetNameValidator
+  #include ActiveModel::Validations
+  # validates_with BudgetNameValidator
   validates :amount, numericality: true
   belongs_to :user
   has_many :purchases
   has_many :cards, through: :purchases
+  scope :current_budgets, -> {where(month: Date.today.strftime("%B '%y"))}
 
   def current_budget_months
     year = Date.today.strftime(" '%y")
     months = ['January','February','March','April','May','June','July','August','September','October','November','December']
-    months.each{|m| m << year}
-  end
-
-  def current_budgets
-    current_user.budgets.map{|b| b.name if b.month.include?(Date.today.strftime("%B '%y"))}
+    months.map{|m| m << year}
   end
 
 end
