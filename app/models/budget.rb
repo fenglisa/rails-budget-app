019e6d0a3a57758973da22ad79bf9507d517a3db
fenglisa <lisafeng10@gmail.com>
2020-09-16 12:41:36 -0400

    current month helper refactor

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,19 +1,12 @@
 class Budget < ApplicationRecord
   validates :name, presence: true
   validates :amount, numericality: true
-
   before_validation :normalize_name
 
   belongs_to :user
   has_many :purchases
   has_many :cards, through: :purchases
   scope :current_budgets, -> {where(month: Date.today.strftime("%B '%y"))}
-
-  def current_budget_months
-    year = Date.today.strftime(" '%y")
-    months = ['January','February','March','April','May','June','July','August','September','October','November','December']
-    months.map{|m| m << year}
-  end
 
   def unique_budget?(user,params)
     budget_names = []
