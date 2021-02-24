45734b233fe5d59fd908aff9e607380b3285a1c6
fenglisa <lisafeng10@gmail.com>
2021-02-24 14:31:10 -0500

    comments for Benefit model methods

--- app/models/benefit.rb
+++ app/models/benefit.rb
@@ -3,17 +3,20 @@ class Benefit < ApplicationRecord
   validates :category, presence: true
   belongs_to :card
 
-
+# benefit categories
   def categories
     ['Gas','Groceries','Restaurants','All other purchases']
   end
 
+# prevents adding an already added benefit category
+# used in new benefits form
   def disabled_categories(card)
     card_benefits = []
     card.benefits.each{|b| card_benefits << b.category}
     card_benefits & categories
   end
 
+# common cashback percentages
   def percentage_cash_back
     [1,1.5,2,3,5,6]
   end
